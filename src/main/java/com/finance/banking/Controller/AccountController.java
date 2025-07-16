f7a42db8505c8a2f9b23646caf4d126aec6a4d25
luminous44 <aklemasultanakhi@gmail.com>
2025-07-16 11:10:12 +0600

    added account creation

--- src/main/java/com/finance/banking/Controller/AccountController.java
+++ src/main/java/com/finance/banking/Controller/AccountController.java
@@ -1,10 +1,21 @@
 package com.finance.banking.Controller;
 
+import com.finance.banking.Dto.AccountDto;
 import com.finance.banking.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class AccountController {
     @Autowired
     private  AccountService accountService;
+
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(@RequestBody AccountDto accountDto){
+        AccountDto createdAccount = accountService.createAccount(accountDto);
+        return new ResponseEntity<>(createdAccount, HttpStatus.CREATED);
+    }
 
 }
